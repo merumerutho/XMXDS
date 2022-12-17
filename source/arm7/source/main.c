b5199c00ee2dbf1ee1c837850a285cebe4551673
merumerutho <merutochan@gmail.com>
2022-12-17 18:57:56 +0100

    Fixing main functions / makefiles

--- source/arm7/source/main.c
+++ source/arm7/source/main.c
@@ -1,45 +1,98 @@
+/*---------------------------------------------------------------------------------
+
+	default ARM7 core
+
+		Copyright (C) 2005 - 2010
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+---------------------------------------------------------------------------------*/
 #include <nds.h>
-#include <stdlib.h>
+#include <dswifi7.h>
+#include <maxmod7.h>
+
+//---------------------------------------------------------------------------------
+void VblankHandler(void) {
+//---------------------------------------------------------------------------------
+	Wifi_Update();
+}
+
+
+//---------------------------------------------------------------------------------
+void VcountHandler() {
+//---------------------------------------------------------------------------------
+	inputGetAndSend();
+}
+
+volatile bool exitflag = false;
+
+//---------------------------------------------------------------------------------
+void powerButtonCB() {
+//---------------------------------------------------------------------------------
+	exitflag = true;
+}
 
 //---------------------------------------------------------------------------------
 int main() {
 //---------------------------------------------------------------------------------
-    // clear sound registers
-    dmaFillWords(0, (void*)0x04000400, 0x100);
+	// clear sound registers
+	dmaFillWords(0, (void*)0x04000400, 0x100);
 
-    REG_SOUNDCNT |= SOUND_ENABLE;
-    writePowerManagement(PM_CONTROL_REG, ( readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP );
-    powerOn(POWER_SOUND);
+	REG_SOUNDCNT |= SOUND_ENABLE;
+	writePowerManagement(PM_CONTROL_REG, ( readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP );
+	powerOn(POWER_SOUND);
 
-    readUserSettings();
-    ledBlink(0);
+	readUserSettings();
+	ledBlink(0);
 
-    irqInit();
-    // Start the RTC tracking IRQ
-    initClockIRQ();
-    fifoInit();
-    touchInit();
+	irqInit();
+	// Start the RTC tracking IRQ
+	initClockIRQ();
+	fifoInit();
+	touchInit();
 
-    SetYtrigger(80);
-    installSoundFIFO();
+	mmInstall(FIFO_MAXMOD);
 
-    installSystemFIFO();
+	SetYtrigger(80);
 
-    //irqSet(IRQ_VCOUNT, VcountHandler);
-    //irqSet(IRQ_VBLANK, VblankHandler);
+	installWifiFIFO();
+	installSoundFIFO();
 
-    //irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
+	installSystemFIFO();
 
-    //setPowerButtonCB(powerButtonCB);
+	irqSet(IRQ_VCOUNT, VcountHandler);
+	irqSet(IRQ_VBLANK, VblankHandler);
 
-    // Keep the ARM7 mostly idle
-    /*
-    while (!exitflag) {
-        if ( 0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R))) {
-            exitflag = true;
-        }
-        swiWaitForVBlank();
-    }
-    */
-    return 0;
+	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
+
+	setPowerButtonCB(powerButtonCB);
+
+	// Keep the ARM7 mostly idle
+	while (!exitflag) {
+		if ( 0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R))) {
+			exitflag = true;
+		}
+		swiWaitForVBlank();
+	}
+	return 0;
 }
--- source/arm9/source/main.c
+++ source/arm9/source/main.c
@@ -1,6 +1,35 @@
-#include <nds.h>
 
-int main()
-{
+#include <nds.h>
+#include <stdio.h>
+
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	// touchPosition touch;
+
+	consoleDemoInit();  // setup the sub screen for printing
+
+	iprintf("XMXDS\n");
+	iprintf("merumerutho - sverx\n\n");
+	iprintf("github.com/merumerutho/XMXDS\n");
+    iprintf("github.com/sverx/libxm7/\n");
+
+    while(1) 
+    {
+        
+    };
+    
+    /*
+	while(1) {
+
+		touchRead(&touch);
+		iprintf("\x1b[10;0HTouch x = %04i, %04i\n", touch.rawx, touch.px);
+		iprintf("Touch y = %04i, %04i\n", touch.rawy, touch.py);
+
+		swiWaitForVBlank();
+		scanKeys();
+		if (keysDown()&KEY_START) break;
+	}
+    */
 	return 0;
 }
